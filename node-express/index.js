d2a6db08bb394ea8ce7bc761c7754750b21c562f
Vivek <viveksem92@gmail.com>
2022-01-18 18:23:42 +0530

    Express routes introduction

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,7 @@ const  http = require("http");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 
+const dishRouter = require("./routes/dishRouter");
 
 const app = express();
 app.use(morgan("dev"));
@@ -11,46 +12,8 @@ console.log(static_file_path)
 app.use(express.static(static_file_path));
 app.use(bodyparser.json());
 
-app.all("/dishes", (req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader("Content-Type","text/plain");
-    next();
-});
+app.use("/dishes", dishRouter);
 
-app.get("/dishes", (req,res,next)=>{
-    res.end("Will send all the dishes to you!");
-});
-
-app.put("/dishes", (req,res,next)=>{
-    res.statusCode = 403
-    res.end("Put operaion not supported");
-});
-
-app.delete("/dishes", (req,res,next)=>{
-    res.end("Deleting all the dishes");
-});
-
-app.post("/dishes", (req,res,next)=>{
-    res.end("Will add dish: "+ req.body.name+
-        " with details "+ req.body.description);
-});
-
-app.get("/dishes/:dishId", (req,res,next)=>{
-    res.end("Will send ditails od dish: "+req.params.dishId+" to you");
-});
-
-app.post("/dishes/:dishId", (req,res,next)=>{
-    res.statusCode = 403
-    res.end("Post operaion not supported on dish: "+req.params.dishId);
-});
-
-app.put("/dishes/:dishId", (req,res,next)=>{
-    res.end("Will update the dish: "+req.params.dishId+" with details "+ req.body.description);
-});
-
-app.delete("/dishes/:dishId", (req,res,next)=>{
-    res.end("Deleting dish: "+ req.params.dishId);
-});
 
 const hostname = "localhost";
 const port = 3000;
