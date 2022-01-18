1633b124eb63c1092f00d6e4485be3eacc5cb3eb
Vivek <viveksem92@gmail.com>
2022-01-18 17:34:21 +0530

    Express server to server static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const  http = require("http");
+const morgan = require("morgan");
+
+
+const app = express();
+app.use(morgan("dev"));
+var static_file_path = __dirname+"/public"
+console.log(static_file_path)
+app.use(express.static(static_file_path));
 
 const hostname = "localhost";
 const port = 3000;
-
-const app = express();
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
